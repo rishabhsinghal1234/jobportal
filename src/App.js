0d5d2d92d0a43866cec0c9468a3758fc7b4f9871
Singhal <rishabh.singhal@cybergindia.local>
2020-11-07 18:00:51 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import com_img2 from './images/IBM_logo.png'
+import com_img from './images/IBM_image.jpg'
+import NavBar from './Component/Navbar';
 import './App.css';
+
+const companyLogoData = [
+  {"Company":com_img, "Logo":com_img2},
+  {"Company":com_img, "Logo":com_img2},
+  {"Company":com_img, "Logo":com_img2},
+  {"Company":com_img, "Logo":com_img2},
+  {"Company":com_img, "Logo":com_img2}
+];
+
+const companyList = companyLogoData.map((data, key) => 
+  <div class="companyImage">
+    <a href="www.google.com">
+      <img src={data.Company} key={key} alt="Forest" width="320" height="320" />
+    </a>
+    <span class="companyLogo">
+      <img src={data.Logo} key={key} alt="Forest" width="180" height="50" />
+    </span>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <div id="top">
+        <h1>Growth on Your mind?</h1>
+        <h4>Explore</h4>
+      </div>
+      <div id="companies">
+        <h2>companies</h2>
+        <div class="company">
+        {companyList}
+      </div>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import NavBar from './Component/Navbar'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
